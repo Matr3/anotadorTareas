47e6f1734dec499eb87472063c76cc8e03831e6f
Matr3 <tremari.marce@gmail.com>
2022-07-25 17:12:54 -0300

    Clase05 completada

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-( () => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
@@ -19,33 +20,14 @@ const createTask = (evento) => {
 
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content = `
-    	
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
+ 
     //task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon())
     list.appendChild(task);
 
 
 };
 
-
-
 //listener (escucha la accion)
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    return i;
-}
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-    
-}
-})();
